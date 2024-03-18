350885e4a9a7370f2a66f15dd9ffddfe9c699f92
pedri <pedri@PEDROBOOK2>
2024-03-17 21:31:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,73 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Hogwarts",
+                Endereco = "Escócia"
+            },
+            new Instituicao()
+            {
+                InstituicaoID= 2,
+                Nome = "Mansao X",
+                Endereco = "Escocia"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+        public IActionResult Create() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Instituicao instituicao)
+        {
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Instituicao instiuicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instiuicao.InstituicaoID).First());
+            instituicoes.Add(instiuicao);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academico.Models
+{
+    public class Departamento
+    {
+        [Key] // Anotação para indicar que é a chave primária
+        public long DepartamentoID { get; set; }
+
+        // Campo Nome do tipo string
+        [Required] // Anotação para indicar que o campo é obrigatório
+        [StringLength(100)] // Anotação para limitar o tamanho máximo do nome
+        public string Nome { get; set; }
+    }
+}
